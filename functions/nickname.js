e559d66747e9627b6b37a048c3103ce2d730196a
h1dm <corriolgamer@gmail.com>
2022-02-08 09:57:06 +0200

    Fixing + logs

--- functions/nickname.js
+++ functions/nickname.js
@@ -1,3 +1,7 @@
+const Discord = require('discord.js')
+const mojang = require('mojang-api')
+const fetch = require('node-fetch')
+
 module.exports.run = async(client,message,args) => {
 	let input = 'https://api.mojang.com/user/profile/agent/minecraft/name/' + args[1]
 	let res = fetch(input)
---
+++ logs.bat
@@ -0,0 +1,2 @@
+@echo off
+heroku logs -a nevercord-bot
