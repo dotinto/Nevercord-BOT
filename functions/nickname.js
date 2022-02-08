8a6773fc8df025f4bac95585ad01519703abeb90
h1dm <corriolgamer@gmail.com>
2022-02-08 11:07:16 +0200

    Update nickname.js

--- functions/nickname.js
+++ functions/nickname.js
@@ -8,17 +8,14 @@ module.exports.run = async(client,message,args) => {
 	let res = fetch(info, history)
 		.then(res => res.json())
 		.then(json => {
-				let res = fetch(info, history)
-					.then(res => res.json())
-					.then(hst => {
-						const embed = new Discord.MessageEmbed()
-			    		.setColor('#2F3136')
-			    		.setTitle('Информация о игроке Minecraft по нику')
-			    		.addField('Никнейм', `${json.name}`)
-			    		.addField('UUID', `${json.id}`)
-			    		.setDescription('История никнеймов', hst.name)
-						message.channel.send(embed);
-				})
+				const embed = new Discord.MessageEmbed()
+			    .setColor('#2F3136')
+			    .setTitle('Информация о игроке Minecraft по нику')
+			    .addField('Никнейм', `${json.name}`)
+			    .addField('UUID', `${json.id}`)
+			    .setDescription('История никнеймов', history.name)
+				
+				message.channel.send(embed);
 			})
 	if (args[1] === null) return;
 }
