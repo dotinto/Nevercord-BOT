ed7c2ea3396a3742461be7e51f8dba79a2698324
h1dm <corriolgamer@gmail.com>
2022-02-08 11:04:04 +0200

    Update nickname.js

--- functions/nickname.js
+++ functions/nickname.js
@@ -3,17 +3,22 @@ const mojang = require('mojang-api')
 const fetch = require('node-fetch')
 
 module.exports.run = async(client,message,args) => {
-	let input = 'https://api.mojang.com/user/profile/agent/minecraft/name/' + args[1]
-	let res = fetch(input)
+	let info = 'https://api.mojang.com/user/profile/agent/minecraft/name/' + args[1]
+	let history = 'https://api.mojang.com/user/profile/' + args[1] +'/names'
+	let res = fetch(info, history)
 		.then(res => res.json())
 		.then(json => {
-				const embed = new Discord.MessageEmbed()
-			    .setColor('#2F3136')
-			    .setTitle('Информация о игроке Minecraft по нику')
-			    .addField('Никнейм', `${json.name}`)
-			    .addField('UUID', `${json.id}`)
-				
-				message.channel.send(embed);
+				let res = fetch(info, history)
+					.then(res => res.json())
+					.then(hst => {
+						const embed = new Discord.MessageEmbed()
+			    		.setColor('#2F3136')
+			    		.setTitle('Информация о игроке Minecraft по нику')
+			    		.addField('Никнейм', `${json.name}`)
+			    		.addField('UUID', `${json.id}`)
+			    		.setDescription('История никнеймов', hst.name)
+						message.channel.send(embed);
+				})
 			})
 	if (args[1] === null) return;
 }
