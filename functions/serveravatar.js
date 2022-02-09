5fa262a57e629cf8e12eebdf2cf0c3594ed8556a
h1dm <corriolgamer@gmail.com>
2022-02-09 09:21:25 +0200

    Create serveravatar.js

---
+++ functions/serveravatar.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const guild = new Discord.Guild();
+
+module.exports.run = async(client,message,args) => {
+	const embed = new Discord.MessageEmbed()
+	.setTitle(`Аватар сервера ${message.guild.name}`)
+	.setImage(message.guild.iconURL())
+	.setColor('#2F3136')
+	.setTimestamp()
+    .setFooter(`${client.user.username}`, client.user.avatarURL())
+	message.channel.send(embed)
+}
+
+module.exports.help = {
+	name: 'serveravatar'
+}
