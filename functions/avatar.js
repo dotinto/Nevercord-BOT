760398c9527d048068608aa716687eedcde9b1ac
h1dm <corriolgamer@gmail.com>
2022-02-09 09:30:38 +0200

    Update avatar.js

--- functions/avatar.js
+++ functions/avatar.js
@@ -4,7 +4,7 @@ module.exports.run = async(client,message,args) => {
 	const Member = message.mentions.users.first() || message.author;
 	const embed = new Discord.MessageEmbed()
 	.setTitle(`Аватар ${Member.username}`)
-	.setImage(Member.displayAvatarURL() + '?size=1024')
+	.setImage(Member.displayAvatarURL() + '?size=1536')
 	.setColor('#2F3136')
 	message.channel.send(embed)
 }
