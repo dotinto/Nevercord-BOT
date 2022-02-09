ce06a21a4cb9e54d1e91a6ae21e9feb8eee5c778
h1dm <corriolgamer@gmail.com>
2022-02-09 13:27:26 +0200

    server

--- functions/help.js
+++ functions/help.js
@@ -8,7 +8,7 @@ module.exports.run = async(client,message,args) => {
     .setFooter(`${client.user.username}`, client.user.avatarURL())
 	.addField('Источники информации', '**>platform** - платформа бота\n**>about** - о боте\n**>userinfo** - информация о пользователе\n**>serverinfo** - информация о сервере', true)
 	.addField('RP команды', '**>kill** - убить\n**>wasted** - "*потрачено*"\n**>suicide** - самоубийство\n**>hug** - обнять\n**>pat** - погладить\n**>wink** - подмигнуть\n**>me** - действие', true)
-	.addField('Утилиты', '**>avatar** - узнать аватар пользователя\n**>say** - сказать от имени бота', true)
+	.addField('Утилиты', '**>avatar** - узнать аватар пользователя\n**>servericon** - узнать иконку сервера\n**>say** - сказать от имени бота', true)
 	.addField('Животные', '**>bird**\n**>cat**\n**>dog**\n**>fox**\n**>kangaroo**\n**>koala**\n**>panda**\n**>raccoon**\n**>redpanda**', true)
 	.addField('Генераторы', '**>coin** - подкинуть монетку', true)
 	.addField('Minecraft команды', '**>nickname** - информация по нику\n**>uuid** - информация по ID', true)
