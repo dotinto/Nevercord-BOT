78cf3ce10ba9b45f2e183dd4e711e90c955812a9
h1dm <corriolgamer@gmail.com>
2022-02-09 16:26:20 +0200

    Update usage.js

--- functions/usage.js
+++ functions/usage.js
@@ -6,12 +6,12 @@ module.exports.run = async(client,message,args) => {
 	.setColor('#2F3136')
 	.setTimestamp()
     .setFooter(`${client.user.username}`, client.user.avatarURL())
-	.addField('Источники информации', '**>platform**\n**>about**\n**>userinfo @пользователь**\n**>serverinfo**\n**>roleinfo @роль**\n**>usage**', true)
-	.addField('RP команды', '**>kill @пользователь**\n**>wasted @пользователь**\n**>suicide @пользователь**\n**>hug @пользователь**\n**>pat @пользователь**\n**>wink @пользователь**\n**>me**', true)
-	.addField('Утилиты', '**>avatar @пользователь**\n**>servericon**\n**>say <сообщение>**', true)
-	.addField('Животные', '**>bird**\n**>cat**\n**>dog**\n**>fox**\n**>kangaroo**\n**>koala**\n**>panda**\n**>raccoon**\n**>redpanda**', true)
-	.addField('Генераторы', '**>coin**', true)
-	.addField('Minecraft команды', '**>nickname <игровой ник>**\n**>uuid <32 значный id>**', true)
+	.addField('Источники информации', '**>platform**\n**>about**\n**>userinfo **`@пользователь`\n**>serverinfo**\n**>roleinfo **`@роль`\n**>usage**')
+	.addField('RP команды', '**>kill** `@пользователь`\n**>wasted** `@пользователь`\n**>suicide** `@пользователь`\n**>hug** `@пользователь`\n**>pat **`@пользователь`\n**>wink **`@пользователь`\n**>me**')
+	.addField('Утилиты', '**>avatar** `@пользователь`\n**>servericon**\n**>say **`<сообщение>`')
+	.addField('Животные', '**>bird**\n**>cat**\n**>dog**\n**>fox**\n**>kangaroo**\n**>koala**\n**>panda**\n**>raccoon**\n**>redpanda**')
+	.addField('Генераторы', '**>coin**')
+	.addField('Minecraft команды', '**>nickname** `<игровой ник>`\n**>uuid** `<32 значный id>`')
 	message.channel.send(embed)
 }
 
