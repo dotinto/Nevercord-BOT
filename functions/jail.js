a5f9fe912cb7e6a899fd2a81e821c40c9c7e90c9
h1dm <corriolgamer@gmail.com>
2022-02-11 10:26:14 +0200

    Update jail.js

--- functions/jail.js
+++ functions/jail.js
@@ -5,15 +5,13 @@ module.exports.run = async(client,message,args) => {
 	const Target = message.mentions.users.first() || message.author;
 
 	let link = `https://some-random-api.ml/canvas/jail?avatar=${Target.displayAvatarURL()}`
-	const attachment = new Discord.MessageAttachment(link, 'jail.png');
   	const embed = new Discord.MessageEmbed()
     .setDescription(`<@${Target.tag}> был посажен`)
 	.setColor('#2F3136')
 	.setTimestamp()
 	.setAuthor(message.author.tag, message.author.displayAvatarURL())
     .setFooter(`${client.user.username}`, client.user.avatarURL())    
-    .attachFiles(attachment)
-    .setImage('attachment://jail.png')
+    .setImage(link)
   	message.channel.send(embed)
 }
 
