862fc8798d6a79dfa689e6ea29536cbd8f3bf002
h1dm <corriolgamer@gmail.com>
2022-02-12 11:22:46 +0200

    Update usage.js

--- functions/usage.js
+++ functions/usage.js
@@ -21,7 +21,7 @@ module.exports.run = async(client,message,args) => {
 	.addField('>uuid', '`>uuid <id игрока>`')
 	.addField('>nickname', '`>nickname <ник игрока>`')
 	.addField('>jail', '`>jail <@пользователь>`')
-	.addField('>try', '`>try <@пользователь>`')
+	.addField('>try', '`>try <сообщение>`')
 	message.channel.send(embed)
 }
 
