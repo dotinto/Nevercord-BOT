a55646ea04de63ff5d4f4fb9792a286b887fe57a
h1dm <corriolgamer@gmail.com>
2022-02-13 22:01:11 +0200

    Update platform.js

--- functions/platform.js
+++ functions/platform.js
@@ -7,7 +7,7 @@ module.exports.run = async(client,message,args) => {
 	.setColor('#2F3136')
 	.setAuthor(message.author.tag, message.author.displayAvatarURL())
     .setFooter(`${client.user.username}`, client.user.avatarURL())
-	.setDescription(`${platform.name}` + '\n' + `${platform.version}` + '\n' + `${platform.product}` + '\n' + `${platform.os}` + '\n' + `${platform.description}`)
+	.setDescription(`${platform.name}` + '\n' + `${platform.version}` + '\n' + `${platform.product}` + '\n' + `Win32 64-bit` + '\n' + `Node.js 16.14.0 on Win32 64-bit`)
 	message.channel.send(embed)
 }
 module.exports.help = {
