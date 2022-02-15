04165ee6b2935ab592cef5cc366b0dfb292cc727
h1dm <corriolgamer@gmail.com>
2022-02-15 22:39:18 +0200

    cringe = del((all)

--- functions/cat.js
+++ functions/cat.js
@@ -6,19 +6,14 @@ module.exports.run = (client, message, args) => {
 	let res = fetch('https://some-random-api.ml/animal/cat')
 		.then(res => res.json())
 		.then(json => {
-			const translate = require('@vitalets/google-translate-api');
-
-			translate(json.fact, {to: 'ru'}).then(res => {
 				const embed = new Discord.MessageEmbed()
 			    .setColor('#2F3136')
 			    .setTitle(`${message.author.username}, котик :3`)
-		    	.setDescription('<:flag_ru:889170215349407774> Интересный факт: ' + `${res.text}` + '\n\n<:flag_us:889170802635857973> Tip: ' + `${json.fact}`)
 		    	.setImage(json.image)
 		    	.setTimestamp()
 		    	.setAuthor(message.author.tag, message.author.displayAvatarURL())
         		.setFooter(`${client.user.username}`, client.user.avatarURL())
 			message.channel.send(embed);
-			})
 	});
 	
 }
--- functions/panda.js
+++ functions/panda.js
@@ -6,19 +6,14 @@ module.exports.run = (client, message, args) => {
 	let res = fetch('https://some-random-api.ml/animal/panda')
 		.then(res => res.json())
 		.then(json => {
-			const translate = require('@vitalets/google-translate-api');
-
-			translate(json.fact, {to: 'ru'}).then(res => {
 				const embed = new Discord.MessageEmbed()
 			    .setColor('#2F3136')
 			    .setAuthor(message.author.tag, message.author.displayAvatarURL())
 			    .setTitle(`${message.author.username}, пандочка :3`)
-		    	.setDescription('<:flag_ru:889170215349407774> Интересный факт: ' + `${res.text}` + '\n\n<:flag_us:889170802635857973> Tip: ' + `${json.fact}`)
 		    	.setImage(json.image)
 		    	.setTimestamp()
         		.setFooter(`${client.user.username}`, client.user.avatarURL())
 			message.channel.send(embed);
-			})
 	});
 	
 }
