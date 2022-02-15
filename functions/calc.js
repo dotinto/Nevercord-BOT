fc1c46c872e620722c2958ba3834d153dd3732e2
h1dm <corriolgamer@gmail.com>
2022-02-15 23:03:18 +0200

    Update calc.js

--- functions/calc.js
+++ functions/calc.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js')
 
 module.exports.run = async(client,message,args) => {
-	let toint = args.join(" ").slice(5);
-	let res = Math.floor(eval(toint));
+	let toeval = args.join(" ").slice(5);
+	let tofloat = parseFloat(eval(toeval));
+	let res = Math.floor(tofloat);
 	const embed = new Discord.MessageEmbed()
 	.setColor('#2F3136')
 	.setAuthor(message.author.tag, message.author.displayAvatarURL())
-	.setDescription(`${toint} = ${res}`)
+	.setDescription(`${toeval} = ${res}`)
 	.setTimestamp()
     .setFooter(`${client.user.username}`, client.user.avatarURL())
 	message.channel.send(embed)
