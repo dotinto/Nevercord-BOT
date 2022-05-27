b4ec23e0aedb72f33effe5464d6739375f133a96
Int0shiro <corriolgamer@gmail.com>
2022-05-27 12:28:24 +0300

    Update about.js

--- functions/about.js
+++ functions/about.js
@@ -6,6 +6,7 @@ module.exports.run = async(client,message,args) => {
 	.setColor('#2F3136')
 	.setAuthor(message.author.tag, message.author.displayAvatarURL())
 	.addField('Разработчик', 'Intoshiro#5822\n**➥ [[Github]](https://github.com/Int0shiro)**', true)
+	.addField('Пригласить на свой сервер', "[[тык]](https://int0shiro.github.io/redirect/nevercord)")
 	.addField('Платформа', 'Node.JS', true)
 	.addField('Язык программирования', 'JavaScript', true)
 	.addField('Кол-во команд', "`44`", true)
